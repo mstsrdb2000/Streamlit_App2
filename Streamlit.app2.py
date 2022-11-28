32db3e81d70149133c5b58bd28fccc2f23115571
mstsrdb2000 <118580850+mstsrdb2000@users.noreply.github.com>
2022-11-28 16:08:48 -0600

    Update Streamlit.app2.py

--- Streamlit.app2.py
+++ Streamlit.app2.py
@@ -1,11 +1,4 @@
 import streamlit 
 
-stresmlit. titel('my parentes new healthy and importnat diner')
-
-streamlit.title('my parents all best food ever')
-
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+streamlit.dataframe(my_fruit_list)
