24775334b403a5c1e6a64513b33adb4215a220d0
mstsrdb2000 <118580850+mstsrdb2000@users.noreply.github.com>
2022-11-28 15:48:49 -0600

    Update Streamlit.app2.py

--- Streamlit.app2.py
+++ Streamlit.app2.py
@@ -3,6 +3,8 @@ import streamlit
 stresmlit. titel('my parentes new healthy and importnat diner')
 
 streamlit.title('my parents all best food ever')
+
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
